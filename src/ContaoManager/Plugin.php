8b8d020a6fd8fdd49def9e8f63d047f2ce3fc683
Daniel Kiesel <icodr8@gmail.com>
2018-01-29 20:09:29 +0100

    optimized contao manager support

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -1,7 +1,32 @@
 <?php
 
+/*
+ * This file is part of the Crosstabs Bundle.
+ *
+ * (c) Daniel Kiesel <https://github.com/iCodr8>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
 namespace Craffft\CrosstabsBundle\ContaoManager;
 
-class Plugin
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Craffft\CrosstabsBundle\CraffftCrosstabsBundle;
+
+class Plugin implements BundlePluginInterface
 {
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(CraffftCrosstabsBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class])
+                ->setReplace(['crosstabs']),
+        ];
+    }
 }
